5762552bfa83cbb30703a02e8f63c08af6504284
nick-nuck <141324314+nick-nuck@users.noreply.github.com>
2025-03-24 19:29:41 +0700

    сделал 1,2 задание

---
+++ simple_backend/src/task_tracker/data.py
@@ -0,0 +1,3 @@
+tasks = [
+    {'id': 1, 'name': 'pospat', 'status': 'not competed'},
+]
--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,32 @@
 from fastapi import FastAPI
+from data import tasks
 
 app = FastAPI()
 
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks
 
 @app.post("/tasks")
 def create_task(task):
-    pass
+    tasks.append(task)
+    return f'task added'
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, name, status):
+    for task in tasks:
+        if task['id'] == task_id:
+            task['name'] = name
+            task['status'] = status
+            return f'tasks updated'
+    raise ValueError(f'task not found')
+
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for task in tasks:
+        if task['id'] == task_id:
+            tasks.remove(task)
+            return f'task removed'
+    raise ValueError(f'task not found')
+
